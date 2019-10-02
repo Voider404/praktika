2ca855ac44d55f9ec11a6261bec3e7308c261024
Voider_404 <sir4alterno@gmail.com>
2019-10-02 13:24:15 +0300

    sth

--- views/layouts/template.php
+++ views/layouts/template.php
@@ -34,7 +34,7 @@ AppAsset::register($this);
 <?php $this->beginBody() ?>
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-  <a class="navbar-brand" href="<?php echo Url::toRoute('/test/glav');?>"><font size="5" color="red" face="Arial">CoreTrain</font></a> /* */
+  <a class="navbar-brand" href="<?php echo Url::toRoute('/test/glav');?>"><font size="5" color="6C8CD5" face="Arial">CoreTrain</font></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
   <span class="navbar-toggler-icon"></span>
   </button>
